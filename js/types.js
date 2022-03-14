6c6d3cca7e7a51202ef3461c2d4883d67f08d930
Aurélio Gregorio Liessem <aurelio@liessem.com.br>
2022-03-14 09:43:47 -0300

    operadores e funções

--- js/types.js
+++ js/types.js
@@ -1,3 +1,4 @@
+/*
 // type number
 const idade = 28
 const pi = 3.14
@@ -9,3 +10,18 @@ console.log(a/b) // Infinity
 var a = 0
 var b = 0
 console.log(a/b) // NaN
+*/
+
+//Conversões
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
